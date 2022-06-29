e433436932ac8f6939cdf827803dff098d3464ff
Orunto Eniola <oruntoeniola@aol.com>
2022-06-29 12:19:39 +0100

    Router Setup

---
+++ src/pages/StarterPack.jsx
@@ -0,0 +1,9 @@
+const StarterPack = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default StarterPack;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Landing} from '/Landing';
+export { default as Blog} from '/Blog';
+export { default as StarterPack} from '/StarterPack';
+export { default as ErrorPage} from '/ErrorPage';
